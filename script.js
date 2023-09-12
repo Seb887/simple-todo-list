4ee7b4bd075042acf57f4a181e2eea41b75d095f
Seb887 <seb.dev@posteo.de>
2023-09-12 22:58:35 +0200

    create functions: delete todos, counter

--- script.js
+++ script.js
@@ -2,19 +2,29 @@
 
 // VARIABLES
 
-const todoItems = [];
+let todoItems = [
+  { id: 1694376860080, text: 'task1', completed: false },
+  { id: 1694376860081, text: 'task2', completed: false },
+  { id: 1694376860082, text: 'task3', completed: false },
+  { id: 1694376860082, text: 'task4', completed: true },
+  { id: 1694376860082, text: 'task4', completed: true },
+  { id: 1694376860082, text: 'task4', completed: true },
+  { id: 1694376860082, text: 'task4', completed: true },
+  // { id: 1694376860083, text: 'taskcomplete', completed: true },
+];
 const newTodo = document.querySelector('.newTodo');
 const todoList = document.querySelector('.todos');
 const uncompletedTodosDiv = document.querySelector('.uncompleted-todos');
 const completedTodosDiv = document.querySelector('.completed-todos');
 const completedTitle = document.querySelector('.completed-title');
+const removeTodo = document.querySelectorAll('.remove-todo');
 
 // FUNCTIONS
 
 function createNewTodoItem(todo) {
   const input = document.createElement('input');
   input.type = 'checkbox';
-  input.classList.add('w-5', 'h-5', 'accent-transparent');
+  input.classList.add('check-todo', 'w-5', 'h-5', 'accent-transparent');
 
   const span = document.createElement('span');
   span.classList.add('flex', 'items-center', 'gap-3');
@@ -23,7 +33,7 @@ function createNewTodoItem(todo) {
   span.appendChild(text);
 
   const icon = document.createElement('i');
-  icon.classList.add('fa-solid', 'fa-xmark');
+  icon.classList.add('remove-todo', 'fa-solid', 'fa-xmark');
 
   const div = document.createElement('div');
   div.classList.add(
@@ -48,7 +58,36 @@ function addTodo(text) {
   refreshUI();
 }
 
-function deleteTodoItem() {}
+function setCompleted(e) {
+  let target = e.target;
+
+  if (target.classList.contains('check-todo')) {
+    target.se;
+    // target.setAttribute('checked');
+  }
+}
+
+function deleteTodoItem(e) {
+  if (e.target.classList.contains('remove-todo')) {
+    // ID des zu löschenden Todos inkl. Wandlung in eine Zahl
+    let todoId = e.target.parentElement.id;
+    todoId = parseInt(todoId);
+
+    todoItems = todoItems.filter((item) => item.id !== todoId);
+
+    console.log(todoItems);
+  }
+
+  refreshUI();
+}
+
+function counter(arr) {
+  const counter = document.querySelector('.counter');
+  const completedCounter = document.querySelector('.completed-counter');
+
+  counter.textContent = todoItems.length;
+  completedCounter.textContent = arr.length;
+}
 
 function refreshUI() {
   // Filtert nach nicht erledigten Aufgaben -> gibt Array zurück
@@ -73,9 +112,13 @@ function refreshUI() {
       completedTodosDiv.appendChild(createNewTodoItem(todo));
     }
   }
+
+  counter(completedTodos);
 }
 
 // EVENTS
+
+window.onload = refreshUI;
 
 newTodo.addEventListener('keyup', (e) => {
   let inputText = e.target.value;
@@ -85,3 +128,6 @@ newTodo.addEventListener('keyup', (e) => {
     newTodo.value = '';
   }
 });
+
+todoList.addEventListener('click', deleteTodoItem);
+todoList.addEventListener('click', setCompleted);
